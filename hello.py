79643563544c5903a760d4c3e4964537775735e1
deq4 <deq4@yandex.ru>
2017-06-25 22:01:11 +0300

    another commit

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def app(environ, start_response):
-    data = bytes(environ['QUERY_STRING'].replace('&', '\n'))
+    data = environ['QUERY_STRING'].replace('&', '\n')
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return data
